4aa1f662180346a9fd5bbb3df4de22ee53ac4211
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2021-01-28 11:57:45 +0100

    Change roles slug migration from VARCHAR 255 to 4

--- database/migrations/2021_01_28_085801_add_slug_roles.php
+++ database/migrations/2021_01_28_085801_add_slug_roles.php
@@ -14,7 +14,7 @@ class AddSlugRoles extends Migration
     public function up()
     {
         Schema::table('roles', function(Blueprint $table) {
-            $table->string('slug', 255);
+            $table->string('slug', 4);
         });
     }
 

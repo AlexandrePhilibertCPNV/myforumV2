f5322c40f32592cd48531f6716361a0bc972f10a
Alexandre Philibert <alexandre.philibert@cpnv.ch>
2021-01-28 10:28:33 +0100

    Add make:adminuser command to php artisan

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +19,27 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/**
+ * Promote a user to admin role
+ */
+Artisan::command('make:adminuser ${id}', function ($id) {
+    $user = User::find($id);
+
+    if (!isset($user)) {
+        $this->info("L'utilisateur $id n'existe pas");
+        return;
+    }
+
+    $admin_role = Role::where('slug', 'ADMI')->first();
+
+    if (!isset($admin_role)) {
+        $this->info("Le role d'administrateur n'existe pas dans la base de donnée");
+        return;
+    }
+
+    $user->role_id = $admin_role->id;
+    $user->save();
+
+    $this->info("$user->pseudo est admin");
+})->purpose('Grants the admin role to as user');
